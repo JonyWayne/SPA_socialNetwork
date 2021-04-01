f8db04fa18b3b6533e232e2e7154e202102df906
JonyWayne <77533155+JonyWayne@users.noreply.github.com>
2021-04-01 12:47:34 +0300

    Chat_WebSocket

--- my-app/src/pages/Chat/ChatPage.tsx
+++ my-app/src/pages/Chat/ChatPage.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import avaPost from '..//..//assets/images/bat.png'
+
+const wsChanel=new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx');
+export type ChatMessageType= {  //Типизацию делаем в соответтвии с документацией сервера
+   message:string,
+   photo: string,
+   userId:number,
+   userName: string,
+
+}
+
 const СhatPage: React.FC=()=> {
 return <div>
     <Chat/>
@@ -7,6 +17,7 @@ return <div>
 }
 
 const Chat:React.FC=()=>{
+    
     return <div>
     <Messages/>
     <AddMessagesForm/>
@@ -15,22 +26,31 @@ const Chat:React.FC=()=>{
 
 
 const Messages: React.FC=()=> {
-    const messages=[1,2,3,4];
-    return <div>
-        {messages.map((m:any)=><Message/>)}
+    const [messages,setMessages]=useState<ChatMessageType[]>([])
+    
+    useEffect(()=> {  //Достаем даные по WebSocket чат с сообщениями
+      wsChanel.addEventListener('message', (e)=>{
+          let newMessages=JSON.parse(e.data)
+          setMessages((prevMessages)=>[...prevMessages, ...newMessages]) //prevMessages вышли из замыкания
+      })  
+    },[])
+    const message=[1,2,3,4];
+    return <div style={{height:'400px',overflowY:'auto'}}>
+        {messages.map((m, index)=><Message key={index} message={m}/>)}
     </div>
     }
 
-  const Message: React.FC=()=>{
-      const message= {
-      url:avaPost,
-      author:'Dimych',
-      text:'Hello Friends',       
-      }
+  const Message: React.FC<{message: ChatMessageType}>=({message})=>{
+    //   const message:ChatMessageType= null;
+    //   {
+    //   url:'https://via.placeholder.com/50',
+    //   author:'Dimych',
+    //   text:'Hello Friends',       
+    //   }
       return <div>
-         <img src={message.url}/> <b>{message.author}</b>
+         <img src={message.photo} style={{width:'30px'}}/> <b>{message.userName}</b>
          <br/>
-         {message.text}
+         {message.message}
           <hr/>
       </div>
 
@@ -38,12 +58,20 @@ const Messages: React.FC=()=> {
 
 
 const AddMessagesForm: React.FC=()=> {
+    const [message,setMessage]=useState('');
+    const sendMessage=()=> {
+            if (!message) {
+            return
+        }
+          wsChanel.send(message)
+          setMessage('')
+    }
     return <div>
     <div>
-       <textarea></textarea>
+       <textarea onChange={(e)=>setMessage(e.currentTarget.value)} value={message}></textarea>
        </div>
        <div>
-       <button>Send</button>
+       <button onClick={sendMessage}>Send</button>
     </div>
     </div>
     }
